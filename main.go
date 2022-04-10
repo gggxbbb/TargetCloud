2582ff834ee43b74df7bedd6188273d331dbabab
gggxbbb <2331490629@qq.com>
2022-04-10 09:20:40 +0800

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
